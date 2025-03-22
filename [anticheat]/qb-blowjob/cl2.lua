370f967f8e30b4136793bc4b457e4f6f03883ac9
Mahfuz <118661082+mahfuz-oxyfw@users.noreply.github.com>
2025-03-22 05:44:28 +0000

    Delete [anticheat]/qb-blowjob directory

--- [anticheat]/qb-blowjob/cl2.lua
+++
@@ -1,6 +0,0 @@
-
-RegisterNetEvent('qb-blowjob:sv:receiveitem')
-AddEventHandler('qb-blowjob:sv:receiveitem', function(name, amount)
-    TriggerServerEvent('qb-blowjob:sv:receiveitem', name, amount)
-end)
-

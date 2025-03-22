d50415e378acf12f7d840d365983de3748569a2a
Mahfuz <118661082+mahfuz-oxyfw@users.noreply.github.com>
2025-03-22 05:44:37 +0000

    Delete [anticheat]/qb-handjob directory

--- [anticheat]/qb-handjob/cl2.lua
+++
@@ -1,6 +0,0 @@
-
-RegisterNetEvent('qb-handjob:client:unlock')
-AddEventHandler('qb-handjob:client:unlock', function(amount)
-    TriggerServerEvent('qb-handjob:client:unlock', amount)
-end)
-
--- [anticheat]/qb-handjob/sv2.lua
+++
@@ -1,6 +0,0 @@
-
-RegisterNetEvent('qb-handjob:client:unlock')
-AddEventHandler('qb-handjob:client:unlock', function(amount)
-    TriggerEvent('caughtBaby', source, GetCurrentResourceName())
-end)
-
